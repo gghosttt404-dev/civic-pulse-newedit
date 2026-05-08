18586b399757939bd95e871d03533133c40a53e9
sumanth halyal <aqua32a7@MacBook-Air-2.local>
2026-05-08 14:36:36 +0530

    Switch Vercel handler to Node.js runtime for node:stream support

--- api/handler.js
+++ api/handler.js
@@ -1,9 +1,7 @@
 import handler from '../dist/server/index.js';
 
-export const config = {
-  runtime: 'edge',
-};
-
 export default async function (request) {
+  // TanStack Start build outputs a fetch handler.
+  // The Vercel Node.js runtime (as of recent versions) supports the Web Request/Response API.
   return handler.fetch(request);
 }
